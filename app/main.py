08cef4856339c2a30941c6db81d79e1c0df5bda2
Alexis-Lijeron <alexislijeron.a@gmail.com>
2024-11-18 10:58:11 -0400

    eliminando archivos innecesarios

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes import cita, usuario, doctor, pago, rol,paciente, generar_3d, subir_foto
+from app.routes import cita, usuario, doctor, pago, rol,paciente, subir_foto
 
 app = FastAPI()
 
